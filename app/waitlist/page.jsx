74126503ab48421999cfbbb96aa04c279927946a
Bhavanish19 <brdhamnaskar@gmail.com>
2025-03-27 22:29:30 -0400

    ]add remote image patterns, and set custom headers for security. Modify waitlist page to include an iframe for external content and refactor component structure

--- app/waitlist/page.jsx
+++ app/waitlist/page.jsx
@@ -3,15 +3,31 @@
 import React from "react";
 import Head from "next/head";
 
-export default function WaitlistPage() {
+// export default function WaitlistPage() {
+//   return (
+//     <>
+//       <Head>
+//         <title>Waitlist</title>
+//       </Head>
+//       <div className="mt-20 fixed w-full">
+//         <div className="aspect-video w-full">Add iframe here</div>
+//       </div>
+//     </>
+//   );
+// }
+
+const WaitlistPage = () => {
   return (
-    <>
-      <Head>
-        <title>Waitlist</title>
-      </Head>
-      <div className="mt-20 fixed w-full">
-        <div className="aspect-video w-full">Add iframe here</div>
+    <div>
+      <div>
+        <iframe
+          style={{ width: "100%" }}
+          height="569"
+          src="https://carpool-waitlist.created.app"
+          title="CarPool Waitlist"
+          frameBorder="0">
+        </iframe>
       </div>
-    </>
+    </div>
   );
-}
+};
